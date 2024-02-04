1b20a319ad6734ee212759cd18bdc0e393ebdeb7
vdunlop <148563390+vdunlop@users.noreply.github.com>
2024-02-04 14:39:16 -0600

    Filled in Models and index.js for the models.

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,14 @@
+// Storage for database variables:
+// DB_NAME, DB_PW and DB_USER
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+// JAWSDB_URL is a MySQL db as a service (DBaaS) provider that supplies
+// functional, fully managed, relational db for use with
+// this application. Use it if my db causes an error??
+
+// create the sequelize class for our use
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
